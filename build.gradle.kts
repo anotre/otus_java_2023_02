ad75fe4eb6a35a10090069ae6d732a0b797ddc3c
anotre <notrevispe@yandex.ru>
2023-05-18 20:29:23 +0500

    Start education otus repository

---
+++ build.gradle.kts
@@ -0,0 +1,82 @@
+import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
+import org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES
+import org.gradle.plugins.ide.idea.model.IdeaLanguageLevel
+
+plugins {
+    idea
+    id("io.spring.dependency-management")
+    id("org.springframework.boot") apply false
+}
+
+idea {
+    project {
+        languageLevel = IdeaLanguageLevel(17)
+    }
+    module {
+        isDownloadJavadoc = true
+        isDownloadSources = true
+    }
+}
+
+
+allprojects {
+    group = "ru.otus"
+
+    repositories {
+        mavenLocal()
+        mavenCentral()
+    }
+
+    apply(plugin = "io.spring.dependency-management")
+    dependencyManagement {
+        dependencies {
+            imports {
+                mavenBom(BOM_COORDINATES)
+            }
+        }
+    }
+    configurations.all {
+        resolutionStrategy {
+            failOnVersionConflict()
+
+            force("javax.servlet:servlet-api:2.4")
+            force("commons-logging:commons-logging:1.1.1")
+        }
+    }
+}
+
+subprojects {
+    plugins.apply(JavaPlugin::class.java)
+    extensions.configure<JavaPluginExtension> {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    tasks.withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        options.compilerArgs.addAll(listOf("-Xlint:all,-serial,-processing", "-Werror"))
+    }
+
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+        testLogging.showExceptions = true
+        reports {
+            junitXml.required.set(true)
+            html.required.set(true)
+        }
+    }
+}
+
+tasks {
+
+    val managedVersions by registering {
+        doLast {
+            project.extensions.getByType<DependencyManagementExtension>()
+                .managedVersions
+                .toSortedMap()
+                .map { "${it.key}:${it.value}" }
+                .forEach(::println)
+        }
+    }
+}
